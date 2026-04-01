bb23c5d835ee6fe2f7782d145aa47401299d5460
Ramana <ramanakellampalli@gmail.com>
2026-03-31 20:07:38 -0500

    fix: add missing AppColors import to scanned_expense_preview.dart

--- lib/presentation/widgets/scanned_expense_preview.dart
+++ lib/presentation/widgets/scanned_expense_preview.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart' show DateFormat, NumberFormat;
 import 'package:provider/provider.dart';
+import '../../core/theme/app_colors.dart';
 import '../../data/models/expense_model.dart';
 import '../../services/receipt_scanner_service.dart';
 import '../state/app_settings_provider.dart';
